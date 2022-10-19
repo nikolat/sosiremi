d7c34b7f199ee3326d646aea9839906c3a08a727
Don <evidence@384.jp>
2022-10-19 10:30:34 +0900

    remove PHP files

--- update.php
+++
@@ -1,9 +0,0 @@
-<?php
-ob_start();
-include 'render.php';
-$html = ob_get_contents();
-ob_end_clean();
-array_map('unlink', glob('repos/*.json'));
-array_map('unlink', glob('repos/*.txt'));
-array_map('unlink', glob('readme/*.txt'));
-file_put_contents(__DIR__. '/docs/index.html', $html);
